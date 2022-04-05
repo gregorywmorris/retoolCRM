3a578fa3aec5b14d09981f4d5c510e29e9d25d55
Gregory W Morris <gwm1980@gmail.com>
2022-04-05 08:57:01 -0400

    Updated bug report and AddSalesOrderData.js

---
+++ AddSalesOrderData.js
@@ -0,0 +1,8 @@
+// AddSalesOrderData
+
+[{
+  'orderid': {{numberInput3.value}}, 'chargeid': '{{textInput14.value}}', 'custid': {{select4.value}}, 'empid': {{select3.value}} , 'freight': {{numberInput2.value.value}} , 'orderdate': {{dateTime1.value}} , 
+'requireddate': {{dateTime2.value}} , 'shipaddress': {{textInput5.value}} , 'shipcity': {{textInput6.value}} , 'shipcountry': {{textInput7.value}} , 'shipregion': {{textInput8.value}} ,'shippostalcode': {{textInput9.value}} ,'shipname': {{textInput10.value}}, 'shipperid': {{select2.value}}, 'shippeddate': {{dateTime3.value}}
+}]
+
+
